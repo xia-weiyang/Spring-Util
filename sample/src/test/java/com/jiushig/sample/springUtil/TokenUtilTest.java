09edda3c7d56c251c6c732e0c5618b7b7456fd98
zk <zkzmzk@163.com>
2018-08-29 10:51:41 +0800

    Fix TokenUtil bug
    (cherry picked from commit 32f9bef)

---
+++ sample/src/test/java/com/jiushig/sample/springUtil/TokenUtilTest.java
@@ -0,0 +1,33 @@
+package com.jiushig.sample.springUtil;
+
+import com.jiushig.springutil.TokenUtil;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by zk on 2018/8/29.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TokenUtilTest {
+
+    private final Logger logger = LoggerFactory.getLogger(TokenUtilTest.class);
+
+    @Before
+    public void init() {
+        TokenUtil.init("test", 10, "test");
+    }
+
+    @Test
+    public void test() {
+        String s = TokenUtil.create(1);
+        logger.info(s);
+        Assert.assertEquals("1", TokenUtil.getByKey(TokenUtil.getToken(s), TokenUtil.KEY_USER_ID));
+    }
+}
